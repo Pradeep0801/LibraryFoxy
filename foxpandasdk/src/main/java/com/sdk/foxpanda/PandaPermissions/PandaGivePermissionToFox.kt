d45ef8ff2718d033caf9c304243347acaf8d2922
SYMBTECH <bhupendra@Bhupendras-MacBook-Air.local>
2018-08-23 13:25:08 +0530

    some changes

--- foxpandasdk/src/main/java/com/sdk/foxpanda/PandaPermissions/PandaGivePermissionToFox.kt
+++ foxpandasdk/src/main/java/com/sdk/foxpanda/PandaPermissions/PandaGivePermissionToFox.kt
@@ -1,25 +1,32 @@
 package com.sdk.foxpanda.PandaPermissions
 
+import android.Manifest
 import android.app.Activity
 import android.os.Bundle
 import android.support.annotation.NonNull
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.sdk.foxpanda.R
 import com.sdk.foxpanda.applications.FoxApplication
 import com.sdk.foxpanda.utils.CommonUtils
 import pub.devrel.easypermissions.EasyPermissions
+import pub.devrel.easypermissions.PermissionRequest
 
-class PandaGivePermissionToFox : Activity(),EasyPermissions.PermissionCallbacks{
+class PandaGivePermissionToFox : AppCompatActivity(),EasyPermissions.PermissionCallbacks,EasyPermissions.RationaleCallbacks{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.background
+        var perms = arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION)
         EasyPermissions.requestPermissions(
                 this,
-                "Location Permission",
+              "Location Permission",
                 PandaShowLocationToFox.RC_LOCATION_PERM,
                 *PandaShowLocationToFox.STORAGE)
+
+
     }
+
 
 
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
@@ -54,4 +61,13 @@ class PandaGivePermissionToFox : Activity(),EasyPermissions.PermissionCallbacks{
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
+
+    override fun onRationaleDenied(requestCode: Int) {
+        finish()
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+
+    }
+
 }
